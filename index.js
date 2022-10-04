fb049b5a6d5daea2c41ba7c40ef396a4d63cb710
Akeno <zzerzv_dev@outlook.com>
2022-10-03 22:04:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -99,6 +99,23 @@ app.get("/", async (req, res) => {
       frameworks: frameworksArray,
     });
   })
+});
+
+
+app.get("/error", (req, res) => {
+  const error = req.query.err;
+  const code = req.query.code;
+  if (error == null || error == "" || error == undefined) {
+    res.json({
+      error: "No error provided"
+    });
+  }
+  else {
+    res.render("error.ejs", {
+      error: error,
+      code: code,
+    })
+  }
 })
 
 app.post("/contact", (req, res) => {
