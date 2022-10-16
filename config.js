354d918826deeff5daa089c7ac3bada59a5c2708
Akeno <zzerzv_dev@outlook.com>
2022-10-16 01:05:02 -0400

    Update config.js
    Signed-off-by: Akeno <zzerzv_dev@outlook.com>

--- config.js
+++ config.js
@@ -1,206 +1,201 @@
 module.exports.webApp = {
     port: 8080,
-    host: "https://akenodev.xyz",
-    isOffline: false,
+    host: "http://localhost",
 },
 
 module.exports.owner = {
-        id: "547923574833545226",
-        supportServer: "https://discord.gg/d9fd8CHWP3",
-        botToken: "MTAyNTExNTI3NTExNDMxOTg4Mw.G83zgK.FzSnNTTS0nvB93U-2bjYKwjsjO7CBxtkk6QhW8",
-        githubToken: "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",
+    id: "YOUR-DISCORD-ID",
+    botToken: "YOUR-DISCORD-BOT-TOKEN",
+    githubToken: "YOUR-GITHUB-TOKEN",
+    supportServer: "YOUR-SUPPORT-SERVER",
+},
 
-        profile: {
-            usename: "akenodev",
-            password: "JS4Life"
+
+module.exports.languages = {
+    "JavaScript": {
+        name: "Jabasukuriputo",
+        options: {
+            percentile: "98"
         }
     },
-
-
-    module.exports.languages = {
-        "JavaScript": {
-            name: "JavaScript",
-            options: {
-                percentile: "98"
-            }
-        },
-        "TypeScript": {
-            name: "TypeScript",
-            options: {
-                percentile: "90"
-            }
-        },
-        "HTML": {
-            name: "HTML",
-            options: {
-                percentile: "69"
-            }
-        },
-        "C#": {
-            name: "C#",
-            options: {
-                percentile: "90"
-            }
-        },
-        "C": {
-            name: "C",
-            options: {
-                percentile: "50"
-            }
-        },
-    };
+    "TypeScript": {
+        name: "TypeScript",
+        options: {
+            percentile: "90"
+        }
+    },
+    "HTML": {
+        name: "HTML",
+        options: {
+            percentile: "69"
+        }
+    },
+    "C#": {
+        name: "C#",
+        options: {
+            percentile: "90"
+        }
+    },
+    "C": {
+        name: "C",
+        options: {
+            percentile: "50"
+        }
+    },
+};
 
 module.exports.frameworks = {
-    React: {
-        name: "React",
-        options: {
-            percentile: "44",
-        },
+React: {
+    name: "Han'nō suru",
+    options: {
+        percentile: "44",
     },
-    Vue: {
-        name: "Vue",
-        options: {
-            percentile: "32",
-        },
+},
+Vue: {
+    name: "Byū",
+    options: {
+        percentile: "32",
     },
-    Angular: {
-        name: "Angular",
-        options: {
-            percentile: "27",
-        },
+},
+Angular: {
+    name: "Kakudo",
+    options: {
+        percentile: "27",
     },
-    EJS: {
-        name: "EJS",
-        options: {
-            percentile: "98",
-        }
-    },
-    Electron: {
-        name: "Electron",
-        options: {
-            percentile: "43",
-        }
+},
+EJS: {
+    name: "EJS",
+    options: {
+        percentile: "98",
     }
+},
+Electron: {
+    name: "Denshi",
+    options: {
+        percentile: "43",
+    }
+}
 };
 
 module.exports.technologies = {
-    Nodejs: {
-        name: "Node.js",
-        options: {
-            fontawesome: "fab fa-node-js",
-        }
-    },
-    MongoDB: {
-        name: "MongoDB",
-        options: {
-            fontawesome: "fas fa-database",
-        }
-    },
-    MySQL: {
-        name: "MySQL",
-        options: {
-            fontawesome: "fas fa-database",
-        }
-    },
-    Redis: {
-        name: "Redis",
-        options: {
-            fontawesome: "fas fa-database",
-        }
-    },
-    Docker: {
-        name: "Docker",
-        options: {
-            fontawesome: "fab fa-docker",
-        }
-    },
-    Git: {
-        name: "Git",
-        options: {
-            fontawesome: "fab fa-git",
-        }
-    },
-    Linux: {
-        name: "Linux",
-        options: {
-            fontawesome: "fab fa-linux",
-        }
-    },
-    Unity: {
-        name: "Unity",
-        options: {
-            fontawesome: "fab fa-unity",
-        }
-    },
-    Npm: {
-        name: "Npm",
-        options: {
-            fontawesome: "fab fa-npm",
-        }
-    },
-    Aws: {
-        name: "AWS",
-        options: {
-            fontawesome: "fab fa-aws",
-        }
+Nodejs: {
+    name: "Node.js",
+    options: {
+        fontawesome: "fab fa-node-js",
     }
+},
+MongoDB: {
+    name: "MongoDB",
+    options: {
+        fontawesome: "fas fa-database",
+    }
+},
+MySQL: {
+    name: "MySQL",
+    options: {
+        fontawesome: "fas fa-database",
+    }
+},
+Redis: {
+    name: "Redis",
+    options: {
+        fontawesome: "fas fa-database",
+    }
+},
+Docker: {
+    name: "Docker",
+    options: {
+        fontawesome: "fab fa-docker",
+    }
+},
+Git: {
+    name: "Git",
+    options: {
+        fontawesome: "fab fa-git",
+    }
+},
+Linux: {
+    name: "Linux",
+    options: {
+        fontawesome: "fab fa-linux",
+    }
+},
+Unity: {
+    name: "Unity",
+    options: {
+        fontawesome: "fab fa-unity",
+    }
+},
+Npm: {
+    name: "Npm",
+    options: {
+        fontawesome: "fab fa-npm",
+    }
+},
+Aws: {
+    name: "AWS",
+    options: {
+        fontawesome: "fab fa-aws",
+    }
+}
 }
 
 
 module.exports.projects = {
-    1: {
-        name: "akenodev.xyz",
-        description: "The Official Github Repo for akenodev.xyz.",
-        url: "https://github.com/akenolol/akenodev.xyz",
-        image: "https://cdn.discordapp.com/icons/1014190469628055552/a_a92a1b63d4ae364ea0e17ad6526bd16b.gif",
-    },
-    2: {
-        name: "Goddess-Bot",
-        description: "The Source Code for The Server Goddess (https://cafe.akenodev.xyz.invite/).",
-        url: "https://github.com/akenolol/Goddess-Bot",
-        image: "https://cdn.discordapp.com/avatars/1006316455886860329/8df894fb0eb61a03200d86d5eaf52867.png?size=1024",
-    },
-    3: {
-        name: "react-todo-app",
-        description: "A simple todo app made with React.",
-        url: "https://github.com/akenolol/react-todo-app",
-        image: "https://cdn.discordapp.com/attachments/981632133191843852/1025205816472653884/3203-reactjs.png",
-    },
-    4: {
-        name: "Basic-Web-Server",
-        description: "A Basic Web Server in nodejs. Built with express.",
-        url: "https://github.com/akenolol/Basic-Web-Server",
-        image: "https://cdn.discordapp.com/attachments/981632133191843852/1025205418194120745/4408_nodejs.png",
-    },
-    5: {
-        name: "c-program",
-        description: "A Simple Program to Show How C Works.",
-        url: "https://github.com/akenolol/c-program",
-        image: "https://cdn.iconscout.com/icon/free/png-512/c-programming-569564.png",
-    },
-    6: {
-        name: "Akeno-Bartender-Bot",
-        description: "The Discord bot For https://discord.gg/g78PTzfMMP",
-        url: "https://github.com/akenolol/Akeno-Bartender-Bot",
-        image: "https://cdn.discordapp.com/attachments/981632133191843852/1025204888944267306/DJNekoWag.gif",
-    }
+1: {
+    name: "akenodev.xyz",
+    description: "The Official Github Repo for akenodev.xyz.",
+    url: "https://github.com/akenolol/akenodev.xyz",
+    image: "https://cdn.discordapp.com/icons/1014190469628055552/a_a92a1b63d4ae364ea0e17ad6526bd16b.gif",
+},
+2: {
+    name: "Goddess-Bot",
+    description: "The Source Code for The Server Goddess (https://cafe.akenodev.xyz.invite/).",
+    url: "https://github.com/akenolol/Goddess-Bot",
+    image: "https://cdn.discordapp.com/avatars/1006316455886860329/8df894fb0eb61a03200d86d5eaf52867.png?size=1024",
+},
+3: {
+    name: "react-todo-app",
+    description: "A simple todo app made with React.",
+    url: "https://github.com/akenolol/react-todo-app",
+    image: "https://cdn.discordapp.com/attachments/981632133191843852/1025205816472653884/3203-reactjs.png",
+},
+4: {
+    name: "Basic-Web-Server",
+    description: "A Basic Web Server in nodejs. Built with express.",
+    url: "https://github.com/akenolol/Basic-Web-Server",
+    image: "https://cdn.discordapp.com/attachments/981632133191843852/1025205418194120745/4408_nodejs.png",
+},
+5: {
+    name: "c-program",
+    description: "A Simple Program to Show How C Works.",
+    url: "https://github.com/akenolol/c-program",
+    image: "https://cdn.iconscout.com/icon/free/png-512/c-programming-569564.png",
+},
+6: {
+    name: "Akeno-Bartender-Bot",
+    description: "The Discord bot For https://discord.gg/g78PTzfMMP",
+    url: "https://github.com/akenolol/Akeno-Bartender-Bot",
+    image: "https://cdn.discordapp.com/attachments/981632133191843852/1025204888944267306/DJNekoWag.gif",
+}
 
 }
 
 module.exports.profile = {
-    aboutMe: ["Voice Actor", "Web Developer", "Discord / Telegram Bot Developer", "Discord Moderator", "Reddit Moderator", "Software Developer", "Anime Voice Actor"],
-    socials: {
-        github: "https://github.com/akenolol",
-        twitter: "https://twitter.com/akeno_dev",
-        reddit: "https://www.reddit.com/user/premage1010",
-        discord: "https://discordapp.com/users/547923574833545226",
-        telegram: "https://t.me/akenodev",
-    },
-    languages: [
-        " en",
-        " jp",
-    ],
+aboutMe: ["Voice Actor", "Web Developer", "Discord / Telegram Bot Developer", "Discord Moderator", "Reddit Moderator", "Software Developer", "Anime Voice Actor"],
+socials: {
+    github: "https://github.com/akenolol",
+    twitter: "https://twitter.com/akeno_dev",
+    reddit: "https://www.reddit.com/user/premage1010",
+    discord: "https://discordapp.com/users/547923574833545226",
+    telegram: "https://t.me/akenodev",
+},
+languages: [
+    " en",
+    " jp",
+],
 
 
 
 }
+
