7ce40c74070ae3e042acbcd724d58eb814259bef
akeno <owner@akenodev.xyz>
2022-10-15 20:41:33 -0400

    Added contextmenu

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,4 +1,3 @@
-
 !function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}(jQuery);
 
 
@@ -7,8 +6,14 @@ function redirect(url) {
 
     if (!url) throw new Error("No URL provided.");
 
-    if (window.confirm("You are being redirected to third party website. Please be aware that we are not responsible for the content of that website. If you wish to continue, click OK.")) {
+    if (window.confirm("You are being redirected to a third party website. Please be aware that we are not responsible for the content of that website. If you wish to continue, click OK.")) {
         window.location.href = url;
     }
     
 }
+
+
+
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
